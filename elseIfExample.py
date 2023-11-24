c32c8ba97f66a5058a5d095a9374c1259a03a4de
Eriel Cabrera <erielcabrera1234@gmail.com>
2023-11-24 17:34:39 -0500

    Else if Example

---
+++ elseIfExample.py
@@ -0,0 +1,17 @@
+name = "Bob"
+age = 3000
+if name == "Alice":
+    print("Hi Alice")
+elif age < 12:
+    print("Yout not Alice")
+elif age > 2000:
+    print("Still older than Alice")
+elif age > 2999:
+    print("Close to Alice's age kiddo")
+
+print(" ")
+
+print("Enter Name: ")
+a = input()
+
+print("Hello " + a + ", Welcome to Miami!")
