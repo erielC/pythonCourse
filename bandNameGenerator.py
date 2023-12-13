6cad15866c238b20b7a42f2de375d2f49fc7ca95
Eriel Cabrera <erielcabrera1234@gmail.com>
2023-12-12 22:11:26 -0500

    Day One

---
+++ bandNameGenerator.py
@@ -0,0 +1,9 @@
+#Band Name Generator
+print("Welcome to the Band Name Generator")
+print("What's the name of the city you grew up in?\n")
+name = input()
+
+print("What's your pet's name?\n")
+petName = input()
+
+print("Your band name could be " + name + " " + petName)
