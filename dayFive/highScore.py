b1c4ed09eee51f3cac2eb07bc5f7f71854e508c3
Eriel Cabrera <erielcabrera1234@gmail.com>
2023-12-17 02:08:37 -0500

    Day Five

---
+++ dayFive/highScore.py
@@ -0,0 +1,15 @@
+# Input a list of student scores
+student_scores = input().split()
+for n in range(0, len(student_scores)):
+  student_scores[n] = int(student_scores[n])
+
+# Write your code below this row 👇
+highScore = 0
+
+for score in student_scores:
+    if score > highScore:
+        highScore = score
+
+# Check for the highest score
+print(f"The highest score in the class is: {highScore}")
+  
