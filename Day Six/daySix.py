2799ef6f0038de2c97868ba88ccb46733c96e20c
Eriel Cabrera <erielcabrera1234@gmail.com>
2023-12-18 19:55:42 -0500

    Day Six

---
+++ Day Six/daySix.py
@@ -0,0 +1,26 @@
+# making your own function 
+
+def myFunction():
+  print("Hello and bye")
+
+myFunction() #printing function 
+
+# While Loop 
+# Robot Game world Problem debug problemworld1 
+# code i used: 
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+
+
+while not at_goal():
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()
+    else: 
+        turn_left()
+  
