2168c78d5f453e348619fe20ea021419d7b07ae0
Eriel Cabrera <erielcabrera1234@gmail.com>
2023-12-30 16:54:41 -0500

    Update CeaserChiperPt4.py

--- Day Eight/CeaserChiperPt4.py
+++ Day Eight/CeaserChiperPt4.py
@@ -3,7 +3,7 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
 def caesar(start_text, shift_amount, cipher_direction):
   end_text = ""
   if cipher_direction == "decode":
-    shift_amount *= -1
+    shift_amount *= -1 
   for char in start_text:
     if char in alphabet: #if there is a number/symbol/or letter not in alphabet list
       position = alphabet.index(char)
