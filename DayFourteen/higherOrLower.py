98f1a7845b860f3326615b1e0278d91907f8fdad
Eriel Cabrera <erielcabrera1234@gmail.com>
2023-12-31 02:39:33 -0500

    Day Fourteen

---
+++ DayFourteen/higherOrLower.py
@@ -0,0 +1,11 @@
+from art import logo, vs
+from gameData import data
+import random
+import os
+
+print(logo)
+
+rChoiceA = random.choice(data)
+rChoiceB = random.choice(data)
+final_Score = 0
+game_Decide = True
