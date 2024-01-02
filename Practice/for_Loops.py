ccb7e7b21bae8398300e4ccea9c7df7e70ecf98d
Eriel Cabrera <erielcabrera1234@gmail.com>
2024-01-01 19:10:47 -0500

    Practice

---
+++ Practice/for_Loops.py
@@ -0,0 +1,12 @@
+# Problem 1: Sum of numbers 1-100
+# total = 0
+# for number in range(1,101):
+#     total += number
+# print(total)
+
+
+# Problem 2: Factorial for Given Number
+# number = int(input())
+# for n in range(1,number):
+#     number *= n
+# print(number)
